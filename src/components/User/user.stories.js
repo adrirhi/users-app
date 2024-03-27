c78e82cab0a58649cccccf32500518266afb2b7b
driss <driss.el.adrirhi@gmail.com>
2024-03-27 14:51:41 +0000

    add stories for user

---
+++ src/components/User/user.stories.js
@@ -0,0 +1,10 @@
+import User from ".";
+
+export default {
+    title: 'components/User',
+    component: User,
+    tags: ['autodocs'],
+  };
+
+
+export const Default = {}
---
+++ src/components/UserRow/UserRow.stories.js
@@ -0,0 +1,14 @@
+import UserRow from ".";
+
+export default {
+    title: 'components/UserRow',
+    component: UserRow,
+    tags: ['autodocs'],
+  };
+
+export const Default = {
+    args: {
+        user: {id: 1, name: "dris"},
+    }
+}
+
